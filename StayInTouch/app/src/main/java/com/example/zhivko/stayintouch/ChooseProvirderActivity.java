7ece8c4daa82610d3dfb7a17a69c1c4cdc9825e5
mafiota7 <jivkoyotov@gmail.com>
2016-04-10 16:20:06 +0300

    activity created

---
+++ StayInTouch/app/src/main/java/com/example/zhivko/stayintouch/ChooseProvirderActivity.java
@@ -0,0 +1,13 @@
+package com.example.zhivko.stayintouch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseProvirderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_provirder);
+    }
+}
